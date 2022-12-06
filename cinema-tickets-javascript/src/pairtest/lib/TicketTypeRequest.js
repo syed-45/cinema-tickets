556c64e9effc20f651002c66892df8f8cee9dde6
Syed <haque_45@hotmail.co.uk>
2022-12-06 15:25:03 +0000

    prevent from buying more than 20 tickets together

--- cinema-tickets-javascript/src/pairtest/lib/TicketTypeRequest.js
+++ cinema-tickets-javascript/src/pairtest/lib/TicketTypeRequest.js
@@ -3,10 +3,17 @@
  */
 
 export default class TicketTypeRequest {
+  #totalTickets = 0
 
   constructor(arrayOfTicketTypesAndQuantities) { //[[type, quantity], [type, quantity]...]
 
     for (let [ticketType, quantity] of arrayOfTicketTypesAndQuantities) {
+
+      this.#totalTickets += quantity
+
+      if (this.#totalTickets > 20) {
+        throw new TypeError(`Sorry you have exceeded the maximum of 20 tickets allowed to purchase at one time`)
+      }
 
       if (!this.#Type.includes(ticketType)) {
         throw new TypeError(`type must be ${this.#Type.slice(0, -1).join(', ')}, or ${this.#Type.slice(-1)}`);
@@ -37,5 +44,16 @@ export default class TicketTypeRequest {
 
 
 
-let newTickets = new TicketTypeRequest([['ADULT', 3],['INFANT',5],['INFANT',5],['CHILD',5]])
-console.log(newTickets.getNoOfTicketsForAType('ADULT'))
+//tests for buying more than 20 tickets
+// let newTickets = new TicketTypeRequest([['ADULT', 3],['INFANT',5],['INFANT',5],['CHILD',5]])
+//=> should be fine
+// let newTickets = new TicketTypeRequest([['ADULT', 3],['INFANT',5],['INFANT',5],['CHILD',5],['CHILD',5]])
+//=> should'nt be fine
+// let newTickets = new TicketTypeRequest([['ADULT', 30]])
+// //=> should'nt be fine
+// let newTickets = new TicketTypeRequest([['CHILD', 30]])
+// //=> should'nt be fine
+// let newTickets = new TicketTypeRequest([['INFANT', 30]])
+// //=> should'nt be fine
+
+console.log(newTickets.getAllTicketTypes())
